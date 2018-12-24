001a59192e63fa015b78e296bba4f3a59a73ce2f
Pratibha Kharel <pk66@cs.washington.edu>
2018-12-23 16:11:49 -0800

    Transaction implemented

--- src/main/java/pratibha/co/repository/impl/TinyUrlRepositoryImpl.java
+++ src/main/java/pratibha/co/repository/impl/TinyUrlRepositoryImpl.java
@@ -1,11 +1,16 @@
 package pratibha.co.repository.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pratibha.co.repository.TinyUrlRepository;
 import pratibha.co.repository.entity.User;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,10 +24,21 @@ public class TinyUrlRepositoryImpl implements TinyUrlRepository {
                 (rs, rowNum) -> new User( rs.getString("userid"), rs.getString("cname"),
                         rs.getString("createdon"))).stream().collect(Collectors.toList());
     }
-
+    @Transactional
     public void createUser(User user){
-        jdbcTemplate.update(
-                "INSERT INTO Users(userid, cname) VALUES (?, ?)",
-                user.getUserId(), user.getCname());
+        try {
+            int count = jdbcTemplate.queryForObject(
+                    "Select count(*) From Users Where userid = ?", Integer.class, user.getUserId());
+            if (count > 0) {
+                throw new ClientErrorException(Response.Status.CONFLICT);
+            }
+            jdbcTemplate.update(
+                    "INSERT INTO Users(userid, cname) VALUES (?, ?)",
+                    user.getUserId(), user.getCname());
+        } catch (DataAccessException ex){
+                ex.printStackTrace();
+                throw new ServerErrorException(Response.Status.SERVICE_UNAVAILABLE);
+        }
+
     }
 }
